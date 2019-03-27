2c90a45e8f9536e516d7b30f997789a1bd7b95f1
Matthew McCormack <mmccormack1182@gmail.com>
2019-03-27 12:50:28 -0400

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :posts, only: [:new, :show, :create, :edit, :update]
+  resources :authors, only: [:new, :show, :create, :edit, :update]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
