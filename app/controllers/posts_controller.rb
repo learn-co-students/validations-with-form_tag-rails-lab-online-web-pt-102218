4d74086f1d38794f82a1b3fccda65645b28fdf6c
github username <github email address>
2019-03-29 15:06:14 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,7 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find_by(params[:id])
-    @post = Post.assign_attributes(params.require(:posts).permit(:title, :category, :content))
+    @post = Post.assign_attributes(post_params(:title, :category, :content))
     if @post.valid?
       @post.update(post_params(:title, :category, :content))
       redirect_to post_path(@post)
