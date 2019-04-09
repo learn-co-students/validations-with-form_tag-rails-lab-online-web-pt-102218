ac176b251d031e7105acc217e24e3afde65c3bad
github username <github email address>
2019-04-09 07:16:16 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :post, only: [:index, :new, :create, :show, :edit, :update]
+  resource :author, only: [:index, :new, :create, :show, :edit, :update]
 end
