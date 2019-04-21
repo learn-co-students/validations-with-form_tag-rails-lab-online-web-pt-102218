b67ce1cdd8cdd916a6270c4bf130e2fc31db563d
Elyse Burmester <burmesterelyse@gmail.com>
2019-04-21 02:40:34 +0000

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -6,11 +6,29 @@ class AuthorsController < ApplicationController
   def new
     @author = Author.new
   end
+  
+  def edit 
+  end 
 
   def create
-    @author = Author.create(author_params)
-
-    redirect_to author_path(@author)
+    @author = Author.new(author_params)
+    
+    if @author.valid? 
+      @author.save 
+      redirect_to author_path(@author)
+    else 
+      render :new 
+    end 
+  end
+  
+  def update 
+    @author = Author.find(params[:id])
+    @author.update(author_params)
+    if @author.valid? 
+      redirect_to author_path(@author)
+    else 
+      render :edit 
+    end 
   end
 
   private
